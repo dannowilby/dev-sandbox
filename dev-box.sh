8a32fa5d7b5b43c828861f526bb633b5c980e65e
dannowilby <dywilby@gmail.com>
2026-09-09 18:34:04 -0700

    initial commit

---
+++ dev-box.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+cmd=$1
+name=$2
+
+if [[ $cmd = "start" ]]; then
+    # provision the dev environment
+    uv run main.py create "$name"
+
+    # get the pod name
+    claim_name=$(cat $name.claim.txt)
+    sandbox_id=$(cat $name.id.txt)
+
+    # start an ssh connection
+    kubectl exec -it "$sandbox_id" -- /bin/bash
+
+    exit 0
+fi
+
+# get the pod name
+claim_name=$(cat $name.claim.txt 2> /dev/null)
+sandbox_id=$(cat $name.id.txt 2> /dev/null)
+
+# clean up
+if [[ $cmd = "stop" ]]; then
+    uv run main.py delete "$claim_name"
+
+    rm $name.claim.txt 2> /dev/null
+    rm $name.id.txt 2> /dev/null
+
+    exit 0
+fi
+
+echo ""
+echo "commands are: start, stop"
+echo ""
+echo "for example:"
+echo "./dev-box.sh start <name>"
+echo "or,"
+echo "./dev-box.sh stop <name>"
+echo ""
---
+++ main.py
@@ -0,0 +1,75 @@
+import sys
+from pathlib import Path
+
+from k8s_agent_sandbox import SandboxClient
+from k8s_agent_sandbox.models import SandboxLocalTunnelConnectionConfig
+
+
+def print_help():
+    print("Explanation of how to use the command")
+
+"""
+main.py <command> <name>
+"""
+def main():
+
+    if len(sys.argv) != 3:
+        print_help()
+        return
+
+    if sys.argv[1] == "create":
+        create_sandbox(sys.argv[2])
+        return
+
+    if sys.argv[1] == "delete":
+        delete_sandbox(sys.argv[2])
+        return
+
+def create_sandbox(name):
+
+    box = Path(name + ".id.txt")
+
+    if box.exists():
+        return    
+
+    print("Creating dev environment", name)
+
+    client = SandboxClient(
+        connection_config=SandboxLocalTunnelConnectionConfig()
+    )
+
+    sandbox = client.create_sandbox(
+        warmpool="sandbox-warmpool",
+        namespace="default",
+    )
+
+    print(sandbox.sandbox_id)
+
+    claim_file_name = name + ".claim.txt"
+    with open(claim_file_name, 'w') as fd:
+        fd.write(sandbox.claim_name)
+
+    pod_file_name = name + ".id.txt"
+    with open(pod_file_name, 'w') as fd:
+        fd.write(sandbox.sandbox_id)
+
+
+def delete_sandbox(name):
+    print("Deleting dev environment", name)
+
+    client = SandboxClient(
+        connection_config=SandboxLocalTunnelConnectionConfig()
+    )
+
+    try:
+        sandbox = client.get_sandbox(
+            claim_name=name,
+            resolve_timeout=2
+        )
+        sandbox.terminate()
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
